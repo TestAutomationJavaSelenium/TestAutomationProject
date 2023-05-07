e4b719cf9df627287024ad90b231b44a42a2d556
gaur673 <gauravchauhan673@gmail.com>
2023-05-07 14:17:14 +0530

    2nd commit after reveiew from bharat sir

--- src/com/company/Session15.java
+++ src/com/company/Session15.java
@@ -6,14 +6,10 @@ public class Session15 {
     public static void main(String[] args){
         Session15 session15=new Session15();
         session15.switchCase();
-
     }
-
     public void switchCase(){
-
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-
         switch (a) {
             case 1:
                 System.out.println("you have selected first option");
